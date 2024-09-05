9b9b6c9c2620d9cec0e9317cadf82c0a29cc7dd2
sunny.park <seolseee@gmail.com>
2024-09-05 16:49:47 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/rookies/MultiPath/MultiPathApplication.java
@@ -0,0 +1,13 @@
+package rookies.MultiPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rookies/MultiPath/MultiPathApplicationTests.java
@@ -0,0 +1,13 @@
+package rookies.MultiPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

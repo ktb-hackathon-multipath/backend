04f24a38f72a4cf74034edd1593d62b0055d1974
sunny.park <seolseee@gmail.com>
2024-09-07 05:06:56 +0900

    [feat] 게임 종류 구현

---
+++ src/main/java/rookies/MultiPath/gametype/entity/GameType.java
@@ -0,0 +1,65 @@
+package rookies.MultiPath.gametype.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rookies.MultiPath.empathybutton.entity.EmpathyButton;
+
+import java.util.List;
+
+@Entity
+@Table(name = "game_types")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GameType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String person;
+
+    @Column
+    private String era;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "game_type_id")  // foreign key로 사용될 column
+    private List<EmpathyButton> empathyButtons;
+
+    @Builder
+    public GameType(String title, String person, String era, List<EmpathyButton> empathyButtons) {
+        this.title = title;
+        this.person = person;
+        this.era = era;
+        this.empathyButtons = empathyButtons;
+    }
+
+    public static GameType createGameType(String title, String person, String era, List<EmpathyButton> empathyButtons) {
+        return GameType.builder()
+                .title(title)
+                .person(person)
+                .era(era)
+                .empathyButtons(empathyButtons)
+                .build();
+    }
+
+    public void updateGameType(String title, String person, String era) {
+        this.title = title;
+        this.person = person;
+        this.era = era;
+    }
+
+    public void addEmpathyButton(EmpathyButton button) {
+        this.empathyButtons.add(button);
+    }
+
+    public void removeEmpathyButton(EmpathyButton button) {
+        this.empathyButtons.remove(button);
+    }
+}
